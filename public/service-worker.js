24e80fa57461acbf3926be4dc22920f7ff6ff1d2
Weston <barnesweston@gmail.com>
2022-07-05 16:45:28 -0500

    create service-worker and declare vars

---
+++ public/service-worker.js
@@ -0,0 +1,20 @@
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = 'v_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+	"./css/styles.css",
+	"./js/idb.js",
+	"./js/index.js",
+	"./manifest.json",
+	"./icons/icon-72x72.png",
+	"./icons/icon-96x96.png",
+	"./icons/icon-128x128.png",
+	"./icons/icon-144x144.png",
+	"./icons/icon-152x152.png",
+	"./icons/icon-192x192.png",
+	"./icons/icon-384x384.png",
+	"./icons/icon-512x512.png",
+];
+

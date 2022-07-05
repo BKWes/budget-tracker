b9faf1f11aba1d95a494fe71163f736a38b27eed
Weston <barnesweston@gmail.com>
2022-07-05 16:53:09 -0500

    add event listeners to install, fetch and delete cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,3 +18,45 @@ const FILES_TO_CACHE = [
 	"./icons/icon-512x512.png",
 ];
 
+// respond with cache resources
+self.addEventListener("fetch", function(e) {
+    console.log("fetch request : " + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            return request || fetch(e.request);
+        })
+    );
+});
+
+// cache resources
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        cahces.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME);
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+// delete old cache
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            // filter out cache names with this app prefix
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            // add current cache to keep list
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeepList.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                }) 
+            );
+        })
+    );
+});
